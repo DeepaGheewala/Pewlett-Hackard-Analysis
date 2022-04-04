649820d1fc478086c78030acae3949f5cc69f9a0
DeepaGheewala <deepaparikh18@gmail.com>
2022-04-03 21:07:32 -0500

    modifying files

--- Queries/Employee_Challenge_starter_code.sql
+++ Queries/Employee_Challenge_starter_code.sql
@@ -1,14 +1,5 @@
--- Use Dictinct with Orderby to remove duplicate rows
--- SELECT DISTINCT ON (______) _____,
--- ______,
--- ______,
--- ______
 
--- INTO nameyourtable
--- FROM _______
--- WHERE _______
--- ORDER BY _____, _____ DESC;
-drop table Retirement_titles
+drop table if exists Retirement_titles
 SELECT distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date 
 INTO Retirement_titles
 from employees e
@@ -16,14 +7,14 @@ Left Join titles t on t.emp_no = e.emp_no
 WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 order by e.emp_no
 
-drop table Unique_titles;
+drop table if exists Unique_titles;
 Select  emp_no, first_name, last_name, title
 into Unique_titles
 from Retirement_titles
 WHERE to_date = ('9999-01-01')
 order by emp_no, to_date desc ;
 
-drop table retiring_titles;
+drop table if exists retiring_titles;
 Select   count(title),title
 into retiring_titles
 from Unique_titles
@@ -32,6 +23,7 @@ order by count(title) desc
 
 --Delivery 2-
 --Mentorship eligible employees-
+drop table if exists mentorship_eligibility;
 SELECT distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, t.title, de.from_date, de.to_date 
 INTO mentorship_eligibility
 from employees e
